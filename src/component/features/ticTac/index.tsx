564636edebddd8a68965de9b7239033394c7d772
Anurag chakravarty <anuragqwerty09@outlook.com>
2026-04-15 20:38:06 +0530

    feat: adding grid

--- src/component/features/ticTac/index.tsx
+++ src/component/features/ticTac/index.tsx
@@ -1,7 +1,21 @@
 
+import styles from './style.module.css'
+export default function TicTacToeComp() {
 
-export default function TicTacToeComp(){
+    // need to display a 3x3 grid s
+    // use array and map over it
+
+    const gridArray = new Array(9).fill(0)
+
     return (<>
-    <div>Tic tac toe render</div>
+        <div>Tic tac toe render</div>
+        <div className={styles.grid}>
+
+            {gridArray.map((gridBlock,index) => {
+                
+                
+                return (<div key={gridBlock}>{index}</div>)
+            })}
+        </div>
     </>)
 }
--- src/component/features/ticTac/types.ts
+++ src/component/features/ticTac/types.ts
@@ -1,3 +1,5 @@
 export interface TicTacToe{
     name:string;
+    score:number;
+
 }
