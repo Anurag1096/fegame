6a08496f26f3c9dab0792f386abf89006b555ff4
Anurag chakravarty <anuragqwerty09@outlook.com>
2026-04-18 12:04:37 +0530

    feat: ui for tic tac tow component

--- src/component/features/ticTac/index.tsx
+++ src/component/features/ticTac/index.tsx
@@ -19,13 +19,12 @@ export default function TicTacToeComp() {
     */
     function handleGridClick(e: React.MouseEvent<HTMLDivElement>, index: number) {
         e.preventDefault()
-
+        if(player.X.moves.includes(index) || player.O.moves.includes(index) ) return 
         // get the mark of current player 
         setPlayer((prev) => ({ ...prev, [currentPlayer]: { ...prev[currentPlayer], moves: [...prev[currentPlayer].moves, index] } }))
         setGrid(prev => [...prev.slice(0, index), currentPlayer, ...prev.slice(index + 1)])
         // after every click change the current player to the other player
         //this place will check for winner or draw.
-        console.log(player)
         setCurrentPlayer(prev => (prev === "X" ? "O" : "X"))
 
     }
@@ -36,7 +35,7 @@ export default function TicTacToeComp() {
 
             {grid.map((gridBlock, index) => {
                 return (<div
-
+                    
                     key={`${gridBlock}${index}`}
                     onClick={(e) => handleGridClick(e, index)}
                 >{gridBlock ? gridBlock : " "}</div>)
