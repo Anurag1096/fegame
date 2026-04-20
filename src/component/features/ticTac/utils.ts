acaa673a6a043356e757ffb8a8d784629e058c99
Anurag chakravarty <anuragqwerty09@outlook.com>
2026-04-20 21:56:58 +0530

    feat:adding wining check function

--- src/component/features/ticTac/utils.ts
+++ src/component/features/ticTac/utils.ts
@@ -1,11 +1,15 @@
 import { CurrentTttPlayer } from "./types"
 interface PropsState{
-    player1:CurrentTttPlayer;
-    player2:CurrentTttPlayer;
+    player:CurrentTttPlayer
 }
+//will recieve one object and check agains the rule
+const wining_moves=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+export function figureOutWinner({player}:PropsState):{player ?:CurrentTttPlayer,success:boolean}{
+if(player.moves.length !== 3) return {success:false}
 
-export function figureOutWinner({player1,player2}:PropsState){
 
+
+return {player,success:true}
 }
 
 
